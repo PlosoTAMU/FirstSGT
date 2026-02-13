1211a9ad91748d9aa21eec32d6905909631be298
PlosoTAMU <truman.porter@tamu.edu>
2026-02-13 14:53:04 -0600

    fixed for ios16 again start over

--- FirstSGT/ContentView.swift
+++ FirstSGT/ContentView.swift
@@ -1,25 +1,9 @@
-//
-//  ContentView.swift
-//  FirstSGT
-//
-//  Created by TP on 2/13/26.
-//
-
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-
     var body: some View {
-        
-        Image("1sgtlogo").resizable()
-        
+        Text("Hello World")
+            .font(.largeTitle)
+            .padding()
     }
-
-    
-}
-
-#Preview {
-    ContentView()
-        
 }
--- FirstSGT/FirstSGTApp.swift
+++ FirstSGT/FirstSGTApp.swift
@@ -1,23 +1,10 @@
-//
-//  FirstSGTApp.swift
-//  FirstSGT
-//
-//  Created by TP on 2/13/26.
-//
-
 import SwiftUI
-import CoreData
 
 @main
 struct FirstSGTApp: App {
-    
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext,
-                              persistenceController.container.viewContext)
         }
     }
 }
